9797102a7c42769d7349723987f8085c330906b2
Nicola Cimmino <nicola.cimmino@gmail.com>
2020-12-29 18:10:30 +0100

    Fix tests

--- tests/Tests.ino
+++ tests/Tests.ino
@@ -1,5 +1,5 @@
 #include <Arduino.h>
-#include <DCDatagrams.h>
+#include <DCServices.h>
 #include <probo.h>
 
 void setup()
@@ -9,35 +9,37 @@ void setup()
     proboInit(&Serial, false);
 }
 
+// TODO: add pre-requiste tests (eg valid vsim)
+
 void testTimeDatagramIsValid()
 {
     startTest("Should build a valid time datagram");
 
     // Arrage
     uint8_t datagram[64];
-    uint8_t expectedDatagram[] = {32, 12, 113, 171, 18, 102, 20, 12, 11, 10, 22, 33};
+    uint8_t expectedDatagram[] = {32, 12, 216, 43, 81, 136, 20, 12, 43, 10, 22, 33};
 
     // Act
-    DCTimeDatagram::buildTimeDatagram(10, 22, 33, 11, 12, 20, datagram);
+    DCTimeDatagram::buildTimeDatagram(10, 22, 33, 1, 11, 12, 20, datagram);
 
     // Assert
-    proboAssertTrue(DCTimeDatagram::verifyCRC(datagram), "CRC Valid");
+    proboAssertTrue(DCTimeDatagram::verifyHMAC(datagram), "HMAC Valid");
     proboAssertBuffersEqual(expectedDatagram, datagram, DC_TIME_DATAGRAM_LEN , "Datagram");
 }
 
 void testInvalidCRCShouldFail()
 {
-    startTest("Should fail validating a CRC");
+    startTest("Should fail validating a HMAC");
 
     // Arrage
     uint8_t datagram[64];
     uint8_t testDatagram[] = {32, 12, 110, 171, 18, 102, 20, 12, 11, 10, 22, 33};
 
     // Act
-    bool crcValid = DCTimeDatagram::verifyCRC(testDatagram);
+    bool crcValid = DCTimeDatagram::verifyHMAC(testDatagram);
 
     // Assert
-    proboAssertFalse(crcValid, "CRC invalid");
+    proboAssertFalse(crcValid, "HMAC invalid");
 }
 
 void loop()
